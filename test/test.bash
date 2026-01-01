94d40f6abab3fa285ccb791eadd0de6916e74c0a
cyanhide <core.editor>
2026-01-02 01:53:58 +0900

    Test

--- test/test.bash
+++ test/test.bash
@@ -3,12 +3,22 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 dir=~
- [ "$1" != "" ] && dir="$1" 
+[ "$1" != "" ] && dir="$1"
 
-cd $dir/ros2_ws
+cd $dir/ros2_ws || exit 1
+
+
 colcon build
-source $dir/.bashrc
-timeout 20 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
+source install/setup.bash
 
-cat /tmp/mypkg.log |
-grep 'Listen: 10'
+timeout 20 ros2 launch mypkg system_monitor.launch.py > /tmp/mypkg.log 2>&1 &
+
+sleep 2
+
+grep 'CPU:' /tmp/mypkg.log >/dev/null
+if [ $? -eq 0 ]; then
+    echo "Test passed: CPU/Memory output found."
+else
+    echo "Test failed: CPU/Memory output not found."
+    exit 1
+fi
