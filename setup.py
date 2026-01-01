a38eadb46994f18aa0e3796d7356a38c58bd73b8
cyanhide <core.editor>
2026-01-02 01:41:30 +0900

    Update

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
-import os
 from glob import glob
 from setuptools import setup
+import os
 
 package_name = 'mypkg'
 
@@ -9,8 +9,7 @@ setup(
     version='0.0.0',
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name), glob('launch/*.launch.py'))
     ],
@@ -18,13 +17,14 @@ setup(
     zip_safe=True,
     maintainer='Hidenori Koseki',
     maintainer_email='s22c1044zk@s.chibakoudai.jp',
-    description='a package for practice',
+    description='CPU/Memory monitoring ROS2 package',
     license='BSD-3-Clause',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'talker = mypkg.talker:main',
-            'listener = mypkg.listener:main',
+            'system_publisher = mypkg.system_publisher:main',
+            'system_listener  = mypkg.system_listener:main',
         ],
     },
 )
+
