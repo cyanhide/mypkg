a5bcdaf310d7b0dba3f0484a5ff20da3017812a3
cyanhide <core.editor>
2026-01-07 22:26:49 +0900

    Update

--- test/test.bash
+++ test/test.bash
@@ -11,4 +11,10 @@
  timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
  cat /tmp/mypkg.log |
+ grep 'Listen:!!!!6!!!!'
+
+ cat /tmp/mypkg.log |
  grep 'Listen: 10'
+
+ cat /tmp/mypkg.log |
+ grep 'Listen:!!!!13!!!!'
