cf1bcaca7dffa19cbd4461e118e918670c22b31b
cyanhide <core.editor>
2026-01-07 21:56:51 +0900

    Update

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -8,8 +8,15 @@ from std_msgs.msg import Int16
 
 
 def cb(msg):
-        global node
-        node.get_logger().info("Listen: %d" % msg.data)
+    global node
+    n = msg.data
+
+
+    if n != 0 and (n % 3 == 0 or '3' in str(n)):
+        node.get_logger().info(f" Listen:!!!! {n}!!!!")
+    else:
+        node.get_logger().info(f"Listen: {n}")
+
 
 rclpy.init()
 node = Node("listener")
--- mypkg/talker.py
+++ mypkg/talker.py
@@ -13,17 +13,17 @@ class Talker():
         nh.create_timer(0.5, self.cb)
 
     def cb(self):
-    msg = Int16()
-    msg.data = self.n
-    self.pub.publish(msg)
+        msg = Int16()
+        msg.data = self.n
+        self.pub.publish(msg)
 
-    # 3の倍数だけ特別表示
-    if self.n != 0 and (self.n % 3 == 0 or '3' in str(self.n)):
-        print(f"count =!!!! {self.n}!!!!")
-    else:
-        print(f"count = {self.n}")
+        # 3の倍数だけ特別表示
+        if self.n != 0 and (self.n % 3 == 0 or '3' in str(self.n)):
+            print(f"count =!!!! {self.n}!!!!")
+        else:
+            print(f"count = {self.n}")
 
-    self.n += 1
+        self.n += 1
 
 rclpy.init()
 node = Node("talker")
